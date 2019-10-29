8346f62ff66ff198cfa81e6e6292714c1b606483
Stewart Dulaney <sdulaney@users.noreply.github.com>
2019-10-29 00:23:11 -0700

    Update files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc ngrok2 tmux.conf bin gitignore_global zshrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc ngrok2 tmux.conf bin gitignore_global zshrc emacs"    # list of files/folders to symlink in homedir
 
 ##########
 
